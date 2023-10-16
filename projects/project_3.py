a2c555fb0a337ff62b593cb9df3f0c27bff34ef6
Vihaan Reddy <vihaanreddy08@gmail.com>
2023-10-16 12:52:54 +0530

    Create project_3.py

---
+++ projects/project_3.py
@@ -0,0 +1,8 @@
+
+circumference = float(input("Enter the circumference of the grassland: "))
+
+radius = circumference / 2
+
+area = 3.14 * (radius ** 2)
+
+print(f"The area of the circular grassland is {area:.2f} square units.")
