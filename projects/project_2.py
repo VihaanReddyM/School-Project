4bac60362f59c1b5afcfe0739f87825970ddac00
Vihaan Reddy <vihaanreddy08@gmail.com>
2023-10-16 12:52:49 +0530

    Create project_2.py

---
+++ projects/project_2.py
@@ -0,0 +1,8 @@
+
+amount = float(input("Enter the amount: "))
+
+discount = 0.03 * amount
+
+amount_after_discount = amount - discount
+
+print(f"The discount on {amount} is {amount_after_discount}.")
